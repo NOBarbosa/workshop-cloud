31afacec8a685d5e6871711a1f479106e922eaa4
Natália Barbosa <nob.ufba@gmail.com>
2021-12-26 13:25:26 -0300

    chip clicavel

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,24 +4,28 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import Chip from '@mui/material/Chip';
 
 function Footer(){
+    const handleClick = () => {
+        console.info('You clicked the Chip.');
+      };
+
     return(
         <footer id='contatos' className='footer-component'>
             <div className='container-rigth'>
-             <div className='pointer'>                
-                <a href='https://www.linkedin.com/in/nobarbosa/' target='_blank' rel="noreferrer"><Chip icon={<LinkedInIcon/>} label="Natália Oliveira"/></a>
+             <div>                
+                <a href='https://www.linkedin.com/in/nobarbosa/' target='_blank' rel="noreferrer"><Chip onClick={handleClick} icon={<LinkedInIcon/>} label="Natália Oliveira"/></a>
              </div>   
-             <div className='pointer'>
-                <a href='https://www.linkedin.com/in/beatriz-de-santana-da-cruz/' target='_blank' rel="noreferrer"><Chip icon={<LinkedInIcon/>} label="Beatriz Santana"/></a>
+             <div>
+                <a href='https://www.linkedin.com/in/beatriz-de-santana-da-cruz/' target='_blank' rel="noreferrer"><Chip onClick={handleClick} icon={<LinkedInIcon/>} label="Beatriz Santana"/></a>
              </div>
-             <div className='pointer'>
-                <a href='https://www.linkedin.com/in/jonathan-bisp/' target='_blank' rel="noreferrer"><Chip icon={<LinkedInIcon/>} label="Jonathan Bispo"/></a>
+             <div >
+                <a href='https://www.linkedin.com/in/jonathan-bisp/' target='_blank' rel="noreferrer"><Chip onClick={handleClick} icon={<LinkedInIcon/>} label="Jonathan Bispo"/></a>
              </div>
-            <div className='pointer'>
-                <a href='https://www.linkedin.com/in/julianecarvalho-/' target='_blank' rel="noreferrer"><Chip icon={<LinkedInIcon/>} label="Juliane Carvalho"/></a>
+            <div >
+                <a href='https://www.linkedin.com/in/julianecarvalho-/' target='_blank' rel="noreferrer"><Chip onClick={handleClick} icon={<LinkedInIcon/>} label="Juliane Carvalho"/></a>
             </div>
             </div>
-            <div className='pointer'>
-                <a href='https://www.instagram.com/gdsc_ufba/' target='_blank' rel="noreferrer"><Chip icon={<InstagramIcon />} label="GDSC UFBA"/></a>
+            <div >
+                <a href='https://www.instagram.com/gdsc_ufba/' target='_blank' rel="noreferrer"><Chip onClick={handleClick} icon={<InstagramIcon />} label="GDSC UFBA"/></a>
             </div>
         </footer>
     )
